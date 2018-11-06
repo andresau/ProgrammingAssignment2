c7b24e750bcc00ecc0e80bb200771a80c954d86c
Andres Aguilar <andres.aguilar.umana@accenture.com>
2018-11-06 13:03:36 -0600

    matrix inversion cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow caching of matrix inversion operations
+## using a matrix wrapper created with makeCacheMatrix
 
-## Write a short comment describing this function
+## Creates a matrix wrapper containing a possible cached inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get, 
+         setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Gets the inverted matrix using the cache if available
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if (!is.null(i)) {
+        message("getting cached inverse")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data)
+    x$setinverse(i)
+    i
 }
